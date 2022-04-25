b09e36a413d29a668196a65f8b4b12e9bda7d597
AbiMangalan <86941986+AbiMangalan@users.noreply.github.com>
2022-04-26 00:48:11 +0530

    axios-1

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,21 +1,13 @@
-const UserModel = require('../models/userModel');
-const jwt = require('jsonwebtoken');
+const axios=require('axios');
 
-const newUser = async function(req,res)
+const getSessionsByDistrict = async function(req,res)
 {
     try
     {
-        let data=req.body;
-        if(Object.keys(data).length!=0)
-        {
-            let newData=await UserModel.create(data);
-            res.status(201).send({status : true,msg : newData});
-        }
-        else
-        {
-            res.status(400).send({status : false,msg : "Bad Request!"});
-            
-        }
+        let distId=req.query.districtID;
+        let date=req.query.date;
+        let data=await axios.get('http://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id='+distId+'&date='+date);
+        res.status(200).send({status : true,data : data.data});    
     }
     catch(err)
     {
@@ -23,91 +15,4 @@ const newUser = async function(req,res)
     }
 };
 
-const loginUser = async function(req,res)
-{
-    try
-    {   
-        let data=req.body;
-        let user=await UserModel.findOne({emailId : data.emailId,password : data.password});
-        if(user!=null)
-        {
-            let token=await jwt.sign({_id : user._id,emailId : user.emailId},'SecretKey');
-            res.status(201).send({status : true,msg : token});
-        }
-        else
-        {
-             res.send({status : false,msg : 'Email ID or Password is Incorrect!'});   
-        }
-    }
-    catch(err)
-    {
-        res.status(500).send({status : false,msg : err});
-    }
-};
-
-const getUserDetails = async function(req,res)
-{
-    try
-    {   
-        let id=req.params.userId;
-        let user=await UserModel.findOne({_id : id,isDeleted : false});
-        if(user==null)
-        {
-            res.status(204).send({status : false,msg : 'User does not exist!'});
-        
-        }    
-        else
-        {
-            res.status(200).send({status : true,msg : user});
-        }
-    }
-    catch(err)
-    {
-        res.status(500).send({status : false,msg : err});
-    }
-};
-
-const updateUserDetails = async function(req,res)
-{
-    try
-    {   
-        let id=req.params.userId;
-        let data=req.body;
-        let user=await UserModel.findOneAndUpdate({_id : id,isDeleted : false},data,{new : true});
-        if(user==null)
-        {
-            res.status(204).send({status : false,msg : 'User does not exist!'});
-        }
-        else
-        {
-            res.status(200).send({status : true,msg : user});
-        }
-    }
-    catch(err)
-    {
-        res.status(500).send({status : false,msg : err});
-    }
-};
-
-const deleteUser = async function(req,res)
-{
-    try
-    {   
-        let id=req.params.userId;
-        let user=await UserModel.findOneAndUpdate({_id : id,isDeleted : false},{isDeleted : true});
-        if(user==null)
-        {
-            res.status(204).send({status : false,msg : 'User does not exist!'});
-        }    
-        else
-        {
-            res.status(200).send({status : true,msg : 'User has been deleted successfully!'});
-        }
-    }
-    catch(err)
-    {
-        res.status(500).send({status : false,msg : err});
-    }
-}
-
-module.exports={newUser,loginUser,getUserDetails,updateUserDetails,deleteUser};
+module.exports={getSessionsByDistrict};
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,13 @@
 const express = require('express');
 const controller=require('../controllers/controller');
-const middleWare=require('../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/users',controller.newUser);
-
-router.post('/login',controller.loginUser);
-
-router.get('/users/:userId',middleWare.tokenAuthenticator,controller.getUserDetails);
-
-router.put('/users/:userId',middleWare.tokenAuthenticator,controller.updateUserDetails);
-
-router.delete('/users/:userId',middleWare.tokenAuthenticator,controller.deleteUser);
+router.get('/getSessionsByDistrict',controller.getSessionsByDistrict);
 
 module.exports = router;
 
 router.get('/test',function (req, res) 
 {
-    res.send('Token-Auth Assignment')
+    res.send('axios-1 Assignment')
 });
