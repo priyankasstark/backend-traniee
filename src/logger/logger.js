52c8f2843f79f1648f539945e812ce435c25e355
Your Name <priyanka27sen@gmail.com>
2022-08-06 00:27:56 +0530

    my 1st node.js assignment

---
+++ src/logger/logger.js
@@ -0,0 +1,9 @@
+const myanme = "priyanka sen"
+
+let welcome = function() {
+    console.log('Welcome to my application. I am ' +myanme+' and a part of FunctionUp Plutonium cohort.')
+
+}
+
+module.exports.name = myanme
+module.exports.welcome = welcome
---
+++ src/validator/formatter.js
@@ -0,0 +1,9 @@
+function ts(){
+    let ts1 = "     priyanka sen       "
+    let ts2 ="PRIYANKA SEN"
+    let ts3 ="priyanka sen"
+    console.log(ts1.trim())
+    console.log(ts2.toLowerCase())
+    console.log(ts3.toUpperCase())
+}
+module.exports.ts =ts
