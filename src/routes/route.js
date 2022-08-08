7aaa63b4c03f0e77e3297c9da60fc0345a6dc205
Your Name <priyanka27sen@gmail.com>
2022-08-08 23:25:42 +0530

    my assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,112 +1,88 @@
-// const express = require('express');
-// const abc = require('../introduction/intro')
-// const router = express.Router();
+router.get('/students', function (req, res){
+    let students = ['Sabiha', 'Neha', 'Akash']
+    res.send(students)
+})
 
-// router.get('/test-me', function (req, res) {
-//     console.log('My batch is', abc.name)
-//     abc.printName()
-//     logger.welcome()
+router.get('/student-details/:name', function(req, res){
+    /*
+    params is an attribute inside request that contains 
+    dynamic values.
+    This value comes from the request url in the form of an 
+    object where key is the variable defined in code 
+    and value is what is sent in the request
+    */
 
-//     res.send('My second ever api!')
-// });
+    let requestParams = req.params
 
-// router.get('/students', function (req, res){
-//     let students = ['Sabiha', 'Neha', 'Akash']
-//     res.send(students)
-// })
+    // JSON strigify function helps to print an entire object
+    // We can use any ways to print an object in Javascript, JSON stringify is one of them
+    console.log("This is the request "+ JSON.stringify(req.Params))
+    let studentName = requestParams.name
+    console.log('Name of the student is ', studentName)
+    let studentDetail = studentName + "" + studentName
+    res.send(studentDetail)
+})
+/////////////////////////////////////////////////////////////////////
+//1st//
+router.get('/get-movies',function(req, res){ //student detail api he 
+    let movies1= ['ENDGAME','THE JUDGE','RESTORATION','SHERLOCK HOLMES']//api is implementation is used to send response for request
+    res.send(movies1)//movies wala iske jese krna he
+})
 
-// router.get('/student-details/:name', function(req, res){
-//     /*
-//     params is an attribute inside request that contains 
-//     dynamic values.
-//     This value comes from the request url in the form of an 
-//     object where key is the variable defined in code 
-//     and value is what is sent in the request
-//     */
+//////////////////////////////////////////////////////////////////////
+//// 2nd/////
 
-//     let requestParams = req.params
-
-//     // JSON strigify function helps to print an entire object
-//     // We can use any ways to print an object in Javascript, JSON stringify is one of them
-//     console.log("This is the request "+ JSON.stringify(requestParams))
-//     let studentName = requestParams.name
-//     console.log('Name of the student is ', studentName)
+router.get('/get-movie/:indexNumber',function(req, res){ //student detail api he 
     
-//     res.send('Dummy response')
-// })
+    let movies=[' ENDGAME ','THE JUDGE','RESTORATION','SHERLOCK HOLMES']
+    let index = req.params.indexNumber;
+    console.log(movies[index])
+     res.send(movies[index])
+})
 
-// module.exports = router;
-const express = require('express');
-const abc = require('../introduction/intro')
-const log=require('../logger/logger')
-const hell=require('../util/helper')
-const formatt=require('../validator/formatter')
-
-const underScore=require('underscore');
-const lodashh=require('lodash');
-const { json } = require('body-parser');
-const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    // console.log('My batch is', abc.name)
-    // abc.printName()
-    // console.log("==================MODULE-1=================")
-    // //module 1
-    // log.kchbhi();
-    // console.log("==================MODULE-2=================")
-    // //module 2
-    // console.log(hell.today());
-    // console.log(hell.month());
-    // console.log(hell.batchIn());
-
-    // const weeksays=["monday","tuesday"]
-    // console.log(underScore.first(weeksays))
+/////////////////////////////////////////////////////////////
+///////    3rd //////////////////////////////////////
+router.get('/get-moviess/:indexNumber',function(req, res){ //student detail api he 
     
-// console.log("==================MODULE-3=================")
-//     //module 3
-//     console.log("The string has been trimmed i.e,"+formatt.trim);
-//     console.log("The string has been converted to lower case i.e, "+formatt.toLower);
-//     console.log("The string has been converted to upper case i.e, "+formatt.toUpper);
-    res.send('My second ever api!')
+    let moviesName=['ENDGAME','THE JUDGE','RESTORATION','SHERLOCK HOLMES']
+    let index = req.params.indexNumber;
 
-
-    //lodash
-const arr=["Januray","February","march","april","May","june","july","August","september","october","november","december"]
-console.log(lodashh.chunk(arr,4))
-
-console.log("=============2=============");
-const odd=[1,3,5,7,9,11,13,15,17,19];
-console.log(lodashh.tail(odd));
-console.log("=============3=============");
-const arr1=[1,2,3,4];
-const arr2=[2,4,7,9];
-const arr3=[4,5,2,8];
-const arr4=[90.5,2,3];
-const arr5=[3,4,2,7];
-
-console.log(lodashh.uniq(arr1,arr2,arr3,arr4,arr5));
-console.log("=============4=============");
-const keyVal=[["TONYSTARK","CAPTAINAMERICA"],["BRUCEBANNER","HAWKAYE"],["THOR","BALCKWIDOW"]]
-console.log(lodashh.fromPairs(keyVal));
-});
-
-router.get('/student-details:name',function(req,res){
-    const students=["PRIYANKA",'RDJ'];
-    console.log("This is the name " + JSON.stringify(req.params))
-    let reqParams=req.params;
-    let studentName=reqParams.name;
-
-    res.send(studentName + " DOWNEY")
+     if(index > moviesName.length){
+        return res.send("use a valid index  ")
+     }else{
+    
+     res.send(moviesName[index])
+     }
 })
 
-router.get('/test-you', function(req, res){
+////////////////////////////////////////////////////////////
+///////////// 4th ////////////////////
+//forth wala idhar he
+router.get('/get-/films',function(req, res){ //student detail api he    
 
-res.send('This is the second routes implementation')
+    let moviesName=[ {"id": 1,"name": "ENDGAME"}, 
+ {"id": 2,"name": "THE JUDGE"}, 
+ {"id": 3,"name": "RESTORATION"},
+  {"id": 4,"name": "SHERLOCK HOLMES"}]
+    res.send(moviesName)
 })
 
-router.get('/test-us',function(req, res){
- 
+//////////////////////////////////////////////////
+////////////// 5th /////////////////////////////////
+router.get('/get-/films/:indexNumber',function(req, res){ //student detail api he    
 
-    res.send('This is test-us router')
+    let moviesName=[ {"id": 1,"name": "INFINITY WAR"}, 
+ {"id": 2,"name": "ENDGAME"}, 
+ {"id": 3,"name": "THE JUDGE"},
+  {"id": 4,"name": "SHERLOCK HOLMES"}]
+    let index = req.params.indexNumber;
+     if(index > moviesName.length){
+        return res.send("no movie exist with this id ")
+     }else{
+     res.send(moviesName[index])
+     }
 })
+
+
+
 module.exports = router;
