8e5377ecaf7f5af698a9d8d07393d626d0c543e3
Your Name <priyanka27sen@gmail.com>
2022-08-13 05:53:31 +0530

    post api solution 1,2,3

--- src/routes/route.js
+++ src/routes/route.js
@@ -69,159 +69,152 @@ router.post("/test-post-4", function(req, res) {
     arr.push(ele)
     res.send(  { msg: arr , status: true }  )
 })
-
-// let players =
-//   [
-//     {
-//       "name": "manish",
-//       "dob": "1/1/1995",
-//       "gender": "male",
-//       "city": "jalandhar",
-//       "sports": [
-//         "swimming"
-//       ]
-//     },
-//     {
-//       "name": "gopal",
-//       "dob": "1/09/1995",
-//       "gender": "male",
-//       "city": "delhi",
-//       "sports": [
-//         "soccer"
-//       ],
-//     },
-//     {
-//       "name": "lokesh",
-//       "dob": "1/1/1990",
-//       "gender": "male",
-//       "city": "mumbai",
-//       "sports": [
-//         "soccer"
-//       ],
-//     },
-//   ]
-
-//   router.post('/players', function (req, res) {
-//     //LOGIC WILL COME HERE
-//     let newplayer = req.body.player
-//     let n = newplayer.name
-  
-//     for (i = 0; i < players.length; i++) {
-//       if (players[i].name == n) {
-//         return res.send("Sorry, This name is already exist!")
-//       }
-//     }
-//     players.push(newplayer)
-//     res.send({players})
-  
-//   })
-
-  
-
-
 let players =
-   [
-       {
-           name: "manish",
-           dob: "1/1/1995",
-           gender: "male",
-           city: "jalandhar",
-           sports: ["swimming"],
-           bookings: [
-               {
-                   bookingNumber: 1,
-                   sportId: "",
-                   centerId: "",
-                   type: "private",
-                   slot: '1234000000',
-                   bookedOn: '31/08/2021',
-                   bookedFor: '01/09/2021'
-               },
-               {
-                   bookingNumber: 2,
-                   sportId: "",
-                   centerId: "",
-                   type: "private",
-                   slot: '12456000000',
-                   bookedOn: '31/08/2001',
-                   bookedFor: '01/09/2001'
-               },
-           ]},
-       {
-           name: "gopal",
-           dob: "1/09/1995",
-           gender: "male",
-           city: "delhi",
-           sports: ["soccer"],
-           bookings: []
-       },
-       {
-           name: "lokesh",
-           dob: "1/1/1990",
-           gender: "male",
-           city: "mumbai",
-           sports: ["soccer"],
-           bookings: [
-       
-            {
-                bookingNumber: 4,
-                sportId: "",
-                centerId: "",
-                type: "private",
-                slot: '131245000000',
-                bookedOn: '31/08/2020',
-                bookedFor: '01/09/2020'
-            }
-        ]
-       }
-   ]
+    [
+        {
+            "name": "manish",
+            "dob": "1/1/1995",
+            "gender": "male",
+            "city": "jalandhar",
+            "sports": [
+                "swimming"
+            ]
+        },
+        {
+            "name": "gopal",
+            "dob": "1/09/1995",
+            "gender": "male",
+            "city": "delhi",
+            "sports": [
+                "soccer"
+            ],
+        },
+        {
+            "name": "lokesh",
+            "dob": "1/1/1990",
+            "gender": "male",
+            "city": "mumbai",
+            "sports": [
+                "soccer"
+            ],
+        },
+    ]
 
-//   
+router.post('/players', function (req, res) {
+    //LOGIC WILL COME HERE
+    let newplayer = req.body
+    let n = newplayer.name
 
-router.post('/player/:playerName/booking/:bookingId',function (request, response){
-    const playerName = request.params.playerName;
-    const bookingId = request.params.bookingId;
-    const bookingData = request.body;
-    bookingData.bookingNumber = Number(bookingId);
+
+
+    for (i = 0; i < players.length; i++) {
+        if (players[i].name == n) {
+            return res.send("Sorry, This name is already exist!")
+        }
+    }
+    players.push(newplayer)
+    res.send(players)
+
+})
+
+let playerbooking = [
+    {
+        bookingid: 1,
+        sportId: 2,
+        centerId: 3,
+        type: "private",
+        slot: 16286598000000,
+        bookedOn: "31/08/2021",
+        bookedFor: "01/09/2021"
+    },
+    {
+        bookingid: 2,
+        sportId: 2,
+        centerId: 3,
+        type: "private",
+        slot: 16286598000000,
+        bookedOn: "31/08/2021",
+        bookedFor: "01/09/2021"
+    }
+
+]
+
+router.post('/player/:playername/booking/:bookingid', (req, res) => {
+    //    console.log(req.params)
+    let flag = false
+    for (let i = 0; i < players.length; i++) {
+        let pla = players[i]
+        if (pla.name === req.params.playername) {
+            flag = true
+        return res.send("Player Is already exist in the panel")
+        }  
+        
+         break;
+    }
+           
+    for (let i = 0; i < playerbooking.length; i++) {
+        let ele = playerbooking[i]
+        if (ele.bookingid == req.params.bookingid) {
+            flag = true
+            // break;
+            return res.send("player is not exist in the panel")
+        }
+    }
+    if (flag == true) {
+        return res.send("already booked")
+    }
+    else {
+        let book = req.body
+        playerbooking.push(book)
+        res.send(playerbooking)
+    }
+
+})
+
+let persons= [
+  {
+  name: "SK",
+  age: 10,
+  votingStatus: false
+},
+{
+  name: "SK",
+  age: 20,
+  votingStatus: false
+},
+{
+  name: "AA",
+  age: 70,
+  votingStatus: false
+},
+{
+  name: "PK",
+  age: 5,
+  votingStatus: false
+},
+{
+  name: "HO",
+  age: 40,
+  votingStatus: false
+}
+]
+
+router.post('/voters',(req,res)=>{
+
+    let param = req.query;
+    let age = parseInt(param.age);
+    let voterArray = [];
     
-    let playerNameStatus = "", bookingIdStatus = "";
-    
-    for(let i = 0; i < players.length; i++){
-    if(players[i].name == playerName){
-    playerNameStatus = i;
-    break;
-    }
-    else{
-    playerNameStatus = null;
-    }
-    }
-    if(playerNameStatus != null){
-    if(players[playerNameStatus].bookings.length == 0){
-    bookingIdStatus = players[playerNameStatus].bookings;
-    }
-    else{
-    for(let i = 0; i < players[playerNameStatus].bookings.length; i++){
-    if(players[playerNameStatus].bookings[i].bookingNumber == bookingId){
-    bookingIdStatus = null;
-    break;
-    }
-    else{
-    bookingIdStatus = players[playerNameStatus].bookings;
-    }
-    }
-    }
-    if(bookingIdStatus != null){
-    bookingIdStatus.push(bookingData);
-    response.send(players);
-    }
-    else{
-    response.send("Booking number already exist !");
-    }
-    }
-    else{
-    response.send("Player name not found !");
-    }
-    });
 
-  
-   module.exports = router;
+   for(let i =0; i < persons.length ; i++){
+      if(persons[i].age >= age){
+           persons[i].votingStatus = true
+          voterArray.push(persons[i])  
+      }
+   }
+
+  res.send(voterArray)
+          
+})
+
+module.exports = router;
