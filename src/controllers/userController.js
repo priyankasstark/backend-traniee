d8affcc879af340d86597384df10aa85c50e81f3
Your Name <priyanka27sen@gmail.com>
2022-08-15 01:30:34 +0530

    updated

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const UserModel= require("../models/userModel")
+const UserModel= require("../models/bookModel")
 
 const createUser= async function (req, res) {
     let data= req.body
@@ -6,10 +6,10 @@ const createUser= async function (req, res) {
     res.send({msg: savedData})
 }
 
-const getUsersData= async function (req, res) {
+const getBooks= async function (req, res) {
     let allUsers= await UserModel.find()
     res.send({msg: allUsers})
 }
 
-module.exports.createUser= createUser
-module.exports.getUsersData= getUsersData
+module.exports.createBooks= createBooks
+module.exports.getBooks= getBooks
