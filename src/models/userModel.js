24b0814fab532cabe61f0ae8414bbd152af404c5
priyankasstark <110596909+priyankasstark@users.noreply.github.com>
2022-08-15 01:38:16 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,30 +1,16 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
+const bookSchema = new mongoose.Schema( {
+    bookName : String,
+    authorName : String,
+    category : {
+        type : String,
+        enum : ["horror","comic","true story"]
     },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
-    // isIndian: Boolean,
-    // parentsInfo: {
-    //     motherName: String,
-    //     fatherName: String,
-    //     siblingName: String
-    // },
-    // cars: [ String  ]
+    year : Number
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+module.exports = mongoose.model('book', bookSchema) //
 
-
-
+// Boolean, Object/json, array
 // Boolean, Object/json, array
