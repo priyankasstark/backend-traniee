a408756523bfaa14ce07582bb8af943508377045
Your Name <priyanka27sen@gmail.com>
2022-08-24 19:21:18 +0530

    assignment done middleware 1st

--- src/index.js
+++ src/index.js
@@ -8,18 +8,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://priyanka99:EorbzmKpqdV7ml9W@cluster0.puozp1a.mongodb.net/Priyanka_007?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
-.catch ( err => console.log(err) )
+.catch ( err => console.log(err) );
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+
+const moment = require('moment');
+let time = moment();
+app.use (function (req, res, next) {
+    let url= req.originalUrl
+    console.log(url)
+    let ip= req.ip
+    console.log(ip)
+    console.log(time.format('h:mm:ss a'));
+    console.log(time.format('YYYY,MM,DD'));
+    next()
+})
 
 app.use('/', route);
 
