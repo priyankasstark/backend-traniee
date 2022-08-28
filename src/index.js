a7550c2af849ad6de11d6912f83babe95f8e5fe3
Your Name <priyanka27sen@gmail.com>
2022-08-28 11:02:10 +0530

    Authentication with JWT

--- src/index.js
+++ src/index.js
@@ -1,35 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+var bodyParser = require('body-parser');
 const { default: mongoose } = require('mongoose');
+const route = require('./routes/route.js');
+const moment = require('moment');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-mongoose.connect("mongodb+srv://priyanka99:EorbzmKpqdV7ml9W@cluster0.puozp1a.mongodb.net/Priyanka_007?retryWrites=true&w=majority", {
-    useNewUrlParser: true
-})
+mongoose.connect("mongodb+srv://priyanka99:EorbzmKpqdV7ml9W@cluster0.puozp1a.mongodb.net/Token_Authentication?retryWrites=true&w=majority", 
+{useNewUrlParser: true})
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) );
 
 
-const moment = require('moment');
-let time = moment();
-app.use (function (req, res, next) {
-    let url= req.originalUrl
-    console.log(url)
-    let ip= req.ip
-    console.log(ip)
-    console.log(time.format('h:mm:ss a'));
-    console.log(time.format('YYYY,MM,DD'));
-    next()
-})
-
 app.use('/', route);
 
-
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
 });
---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        firstName : String,
+        lastName : String,
+        mobile : String,
+        emailId : String,
+        password : String,
+        gender : {
+                    type : String,
+                    enum : ['male','female','other']
+                 },
+        isDeleted : {
+                        type : Boolean,
+                        default : false
+                    },
+        age : Number
+    },{timestamps : true}
+);
+
+module.exports = mongoose.model('users',userSchema);
