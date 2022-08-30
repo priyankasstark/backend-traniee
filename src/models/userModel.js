2974502f7afe2199902b880c3f9fda3554056de2
Your Name <priyanka27sen@gmail.com>
2022-08-30 13:04:35 +0530

    authentication and authorization jwt assignment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema(
-    {
-        firstName : String,
-        lastName : String,
-        mobile : String,
-        emailId : String,
-        password : String,
-        gender : {
-                    type : String,
-                    enum : ['male','female','other']
-                 },
-        isDeleted : {
-                        type : Boolean,
-                        default : false
-                    },
-        age : Number
-    },{timestamps : true}
-);
+const userSchema = new mongoose.Schema( {
+    firstName: String,
+    lastName: String,
+    mobile: {
+        type: String,
 
-module.exports = mongoose.model('users',userSchema);
+        required: true
+    },
+    emailId: String,
+    password: String,
+    gender: {
+        type: String,
+        enum: ["male", "female", "other"]
+    },
+    age: Number,
+    isDeleted :{
+        type : Boolean,
+        default : false
+    },
+    posts: {type: [], deafult: []}
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const controller=require('../controllers/controller');
-const middleWare=require('../middlewares/auth');
-
 const router = express.Router();
+const userController= require("../controllers/userController")
+const middleware = require("../middleware/auth")
+router.get("/test-me", function (req, res) {
+    res.send("My first ever api!")
+})
 
-router.post('/users',controller.newUser);
+router.post("/createusers", userController.createUser)
 
-router.post('/login',controller.loginUser);
+router.post("/loginuser", userController.loginUser , middleware.authenticate )
 
-router.get('/users/:userId',middleWare.tokenAuthenticator,controller.getUserDetails);
+//The userId is sent by front end
+router.get("/getusers/:userId",middleware.authorise, middleware.authorise2, userController.getUserData)
+router.post("/postusers/:userId/posts",middleware.authorise,middleware.authorise2, userController.postMessage)
 
-router.put('/users/:userId',middleWare.tokenAuthenticator,controller.updateUserDetails);
-
-router.delete('/users/:userId',middleWare.tokenAuthenticator,controller.deleteUser);
+router.put("/updateusers/:userId", middleware.authorise ,middleware.authorise2, userController.updateUser)
+ router.delete('/deleteusers/:userId',middleware.authorise ,middleware.authorise2, userController.deleteUser)
 
 module.exports = router;
-
-router.get('/test',function (req, res) 
-{
-    res.send('Token-Auth Assignment')
-});
